5a442b59394619bec7ce2e1a1f2acf61aa98b585
aviad <otmazgin.aviad@gmail.com>
2013-12-13 11:57:55 +0200

    Push test - identation changed

--- emotiondetector/src/main/java/idc/cv/emotiondetector/EyeDetector.java
+++ emotiondetector/src/main/java/idc/cv/emotiondetector/EyeDetector.java
@@ -6,36 +6,43 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 
-public class EyeDetector {
+public class EyeDetector
+{
 
-	private static final EyeDetector	instance	= new EyeDetector();
+    private static final EyeDetector instance = new EyeDetector();
 
-	private EyeDetector() {
-	}
+    private EyeDetector()
+    {
+    }
 
-	public static EyeDetector getInstance() {
-		return instance;
-	}
+    public static EyeDetector getInstance()
+    {
+        return instance;
+    }
 
-	/**
-	 * @throws UnsupportedEncodingException
-	 * */
-	public Pair<Rect, Rect> detectEyes(Mat image) throws Exception {
+    /**
+     * @throws UnsupportedEncodingException
+     */
+    public Pair<Rect, Rect> detectEyes(Mat image) throws Exception
+    {
 
-		MatOfRect eyes = FacePartDetector.getInstance().detect(image, FacePartCascaders.EYE.getClasifier());
+        MatOfRect eyes = FacePartDetector.getInstance().detect(image, FacePartCascaders.EYE.getClasifier());
 
-		if (eyes.toArray().length != 2) {
-			throw new Exception("Found more than two eyes!!");
-		}
+        if (eyes.toArray().length != 2)
+        {
+            throw new Exception("Found more than two eyes!!");
+        }
 
-		Rect first = eyes.toArray()[0];
-		Rect second = eyes.toArray()[1];
+        Rect first = eyes.toArray()[0];
+        Rect second = eyes.toArray()[1];
 
-		if (first.x > second.x) {
+        if (first.x > second.x)
+        {
 
-			return Pair.of(second, first);
-		} else {
-			return Pair.of(first, second);
-		}
-	}
+            return Pair.of(second, first);
+        } else
+        {
+            return Pair.of(first, second);
+        }
+    }
 }
