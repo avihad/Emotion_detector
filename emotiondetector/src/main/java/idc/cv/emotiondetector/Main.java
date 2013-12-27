ac24ca389a1fb35be67e3b47ed7be44b9b866e59
aviad <otmazgin.aviad@gmail.com>
2013-12-27 16:50:02 +0200

    An example of linear regression of chosen points in a smile and a non smile picture

--- emotiondetector/src/main/java/idc/cv/emotiondetector/Main.java
+++ emotiondetector/src/main/java/idc/cv/emotiondetector/Main.java
@@ -102,9 +102,10 @@ public class Main
         }
 
         */
+        //smiling :
         ArrayList<Point> points = new ArrayList<Point>();
 
-        Point base = new Point((mouthRect.x + mouthRect.width / 2), mouthRect.y + mouthRect.height);
+        /*Point base = new Point((mouthRect.x + mouthRect.width / 2), mouthRect.y + mouthRect.height);
 
         points.add(new Point(0, 0));
         Point point1 = normalizeByBase(new Point(mouthRect.x, mouthRect.y), base);
@@ -115,11 +116,55 @@ public class Main
         points.add(point1);
         Utilities.drawLine(new Point(mouthRect.x + mouthRect.width / 3, mouthRect.y + mouthRect.height), new Point(mouthRect.x + mouthRect.width, mouthRect.y), smileImage);
         points.add(point2);
-        points.add(point3);
+        points.add(point3);*/
 
-        ParabolicLinearRegression.linearRegressionOf(points);
+        Point base = new Point(673, 658);
+        Point point1 = new Point(505, 519);
+        points.add(normalizeByBase(point1, base));
+        Point point2 = new Point(557, 612);
+        points.add(normalizeByBase(point2, base));
+        Point point3 = new Point(609, 654);
+        points.add(normalizeByBase(point3, base));
+        Point point4 = new Point(673, 658);
+        points.add(normalizeByBase(point4, base));//base
+        Point point5 = new Point(727, 643);
+        points.add(normalizeByBase(point5, base));
+        Point point6 = new Point(824, 524);
+        points.add(normalizeByBase(point6, base));
+        double smileDeriv = 2*ParabolicLinearRegression.linearRegressionOf(points)[1];
 
-        Utilities.writeImageToFile("problem.png", smileImage);
+        Utilities.drawLine(point1, point2, smileImage);
+        Utilities.drawLine(point2, point3, smileImage);
+        Utilities.drawLine(point3, point4, smileImage);
+        Utilities.drawLine(point4, point5, smileImage);
+        Utilities.drawLine(point5, point6, smileImage);
+        Utilities.writeImageToFile("smileNew.png", smileImage);
+
+        //neutral:
+        Mat neutralImage = Utilities.readImage("/neutral.png");
+        points = new ArrayList<Point>();
+        Point base2 = new Point(663, 626);
+        Point pointN1 = new Point(540, 581);
+        points.add(normalizeByBase(pointN1, base2));
+        Point pointN2 = new Point(619, 626);
+        points.add(normalizeByBase(pointN2, base2));
+        Point pointN3 = new Point(663, 626);        //base
+        points.add(normalizeByBase(pointN3, base2));
+        Point pointN4 = new Point(733, 612);
+        points.add(normalizeByBase(pointN4, base2));
+        Point pointN5 = new Point(753, 607);
+        points.add(normalizeByBase(pointN5, base2));
+        Point pointN6 = new Point(793, 569);
+        points.add(normalizeByBase(pointN6, base2));
+        double neutralDeriv = 2*ParabolicLinearRegression.linearRegressionOf(points)[1];
+
+        System.out.println("Deriv smile: " + smileDeriv + " neutral deriv: " + neutralDeriv + " ratio: " + smileDeriv/neutralDeriv);
+        Utilities.drawLine(pointN1, pointN2, neutralImage);
+        Utilities.drawLine(pointN2, pointN3, neutralImage);
+        Utilities.drawLine(pointN3, pointN4, neutralImage);
+        Utilities.drawLine(pointN4, pointN5, neutralImage);
+        Utilities.drawLine(pointN5, pointN6, neutralImage);
+        Utilities.writeImageToFile("neutralNew.png", neutralImage);
     }
 
     private static Point normalizeByBase(Point point, Point base)
