37af2413fabe470d9093a8891b1e1546c64f2162
aviad <otmazgin.aviad@gmail.com>
2014-01-10 16:52:45 +0200

    Analyzing a whole movie. Here, on 1.avi, using smile picture and neutral pic as an input

--- emotiondetector/src/main/java/idc/cv/emotiondetector/Main.java
+++ emotiondetector/src/main/java/idc/cv/emotiondetector/Main.java
@@ -24,6 +24,15 @@ public class Main
         // Load the native library.
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
+        //analyzeSamples();
+
+        //VideoReader.instance.storeAsFrames("/1.avi");
+
+        analyzeMovie("/neutralInput.png", "/smileInput.png", "/1.avi", 0.002, 0.001);
+    }
+
+    private static void analyzeSamples() throws Exception
+    {
         Mat neutralImage = Utilities.readImage("/neutral2.png");
         Mat smileImage = Utilities.readImage("/smile2.png");
 
@@ -61,6 +70,9 @@ public class Main
         VideoCapture videoCapture = VideoReader.instance.open(movieFilePath);
         int frameNumber = 1;
 
+        int numOfFramesOfSmile = 0;
+        int numOfFramesOfMouthMovement = 0;
+
         while (videoCapture.read(movieFrame))
         {
             Optional<Rect> optionalMouth = MouthDetectorImproved.instance.detectIn(movieFrame);
@@ -74,17 +86,27 @@ public class Main
                 if (mouthCurve >= smileCurve || smileCurve - mouthCurve <= smileThreshold)
                 {
                     System.out.println("Smile is detected at frame number: "+frameNumber);
+                    numOfFramesOfSmile++;
                 }
                 else
                 {
-                    if (mouthCurve > neutralCurve + neutralThreshold)
+                    if (mouthCurve > ((neutralCurve + smileCurve) / 3) + neutralThreshold)
                     {
                         System.out.println("Mouth movement is detected at frame number: "+frameNumber);
+                        numOfFramesOfMouthMovement++;
                     }
                 }
             }
             frameNumber++;
+
+            if (frameNumber % 50 == 0)
+            {
+                System.out.println("Passed: " + frameNumber + " frames");
+            }
         }
+
+        System.out.println("Number of frames with a smile: " + numOfFramesOfSmile);
+        System.out.println("Number of frames with mouth movement: " + numOfFramesOfMouthMovement);
     }
 
     private static double[] smileCurveOf(Mat smileImage, Rect mouth)
